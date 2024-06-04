b7d37817710dca337d0959fb597e12dd2995f687
mattia fiorio <mattia.fiorio@akqa.com>
2024-06-04 11:34:37 +0200

    Finishing up

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,8 +1,8 @@
-import { Form, json, useLoaderData } from "@remix-run/react";
+import { Form, json, useFetcher, useLoaderData } from "@remix-run/react";
 import { useEffect, useState, type FunctionComponent } from "react";
 
-import { getContact, type ContactRecord } from "../data";
-import { LoaderFunctionArgs } from "@remix-run/node";
+import { getContact, updateContact, type ContactRecord } from "../data";
+import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
@@ -105,13 +105,31 @@ export default function Contact() {
   );
 }
 
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+};
+
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, "favorite">;
 }> = ({ contact }) => {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+  /**
+   * The fetcher knows the FormData being submitted to the action,
+   * so it's available to you on fetcher.formData
+   *
+   * The following uses Optimistic UI to set the favourite to true before the request goes through
+   */
+  const favorite = fetcher.formData
+    ? fetcher.formData.get("favorite") === "true"
+    : contact.favorite;
 
+  //Fetcher form does not navigate to another url
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
         name="favorite"
@@ -119,6 +137,6 @@ const Favorite: FunctionComponent<{
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
